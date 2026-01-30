d40efe10b792dbc31b8032e6333f3c369194adda
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-01-30 12:33:27 +0600

    Practiced string methods

---
+++ string.js
@@ -0,0 +1,13 @@
+let firstName = `Farjana  `;
+let lastName =  `  FARJANA   `;
+
+console.log(firstName);
+console.log(lastName);
+/* we use toLowerCase or toUpperCase method to check data's accuracy */
+
+let lowerFirstName = firstName.toLowerCase();
+// console.log(lowerFirstName === firstName.toLowerCase());
+
+/* trim method helps to remove unnecessary or unintentional spaces at the starting or end for reliable storage and display. */
+
+// console.log(firstName.trim() + lastName.trim())

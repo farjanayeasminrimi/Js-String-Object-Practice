98c58601ec6ffc7ff9cab3ddcf87093eba7d1b94
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-01-30 21:03:14 +0600

    Practiced string methods

--- string.js
+++ string.js
@@ -1,8 +1,8 @@
 let firstName = `Farjana  `;
 let lastName =  `  FARJANA   `;
 
-console.log(firstName);
-console.log(lastName);
+// console.log(firstName);
+// console.log(lastName);
 /* we use toLowerCase or toUpperCase method to check data's accuracy */
 
 let lowerFirstName = firstName.toLowerCase();
@@ -11,3 +11,27 @@ let lowerFirstName = firstName.toLowerCase();
 /* trim method helps to remove unnecessary or unintentional spaces at the starting or end for reliable storage and display. */
 
 // console.log(firstName.trim() + lastName.trim())
+
+
+let fullName =  `Farjana Yeasmin Rimi`;
+const tagString = "JavaScript, Web Development, Programming, Front End";
+
+/* Slice is used to show a portion of a string to the user such as in whatsapp text preview, we can see half of the text as an overview*/
+
+let middleName = fullName.slice(8, 15);
+// console.log(fullName);
+// console.log(middleName);
+
+/* Split method is used to transform a string to an array*/
+// console.log(fullName.split(` `));
+
+const arrayString = ['JavaScript', 'Web Development', 'Programming', 'Front End'];
+/* Join method is used to transform an array elements to strings*/
+// console.log(arrayString.join(' | '));
+
+const name =  `Farjana`;
+const sureName = `Rimi`;
+/* Concat method is  used to joint two name from user input */
+let concatName = name.concat(' ', sureName);
+// console.log(concatName);
+
